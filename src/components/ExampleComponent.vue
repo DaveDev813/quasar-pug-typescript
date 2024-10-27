37a88bb88de9d355aae829a442827ceda6743321
Rodave Joseph Bobadilla <rodave.developer@gmail.com>
2024-10-27 15:47:34 +0800

    Apply pug typescript compiler

--- src/components/ExampleComponent.vue
+++ src/components/ExampleComponent.vue
@@ -1,30 +1,28 @@
-<template>
-  <div>
-    <p>{{ title }}</p>
-    <ul>
-      <li v-for="todo in todos" :key="todo.id" @click="increment">
-        {{ todo.id }} - {{ todo.content }}
-      </li>
-    </ul>
-    <p>Count: {{ todoCount }} / {{ meta.totalCount }}</p>
-    <p>Active: {{ active ? 'yes' : 'no' }}</p>
-    <p>Clicks on todos: {{ clickCount }}</p>
-  </div>
+<template lang="pug">
+  div
+    p {{ title }}
+    ul
+      li(v-for='todo in todos' :key='todo.id' @click='increment')
+        | {{ todo.id }} - {{ todo.content }}
+    p Count: {{ todoCount }} / {{ meta.totalCount }}
+    p Active: {{ active ? 'yes' : 'no' }}
+    p Clicks on todos: {{ clickCount }}
+
 </template>
 
 <script setup lang="ts">
 import { computed, ref } from 'vue';
-import { Todo, Meta } from './models';
+import type { Todo, Meta } from './models';
 
 interface Props {
   title: string;
   todos?: Todo[];
   meta: Meta;
   active: boolean;
-};
+}
 
 const props = withDefaults(defineProps<Props>(), {
-  todos: () => []
+  todos: () => [],
 });
 
 const clickCount = ref(0);
